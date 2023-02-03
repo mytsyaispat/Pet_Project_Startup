d0d9f69eab419a2b53792348c7f048cca21974dd
Daniil Korotkov <mytsyaispat@gmail.com>
2023-02-03 18:43:23 +0300

    Change Category

--- src/main/java/com/startup/entity/Category.java
+++ src/main/java/com/startup/entity/Category.java
@@ -7,9 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Cacheable
-@org.hibernate.annotations.Cache(usage =
-        CacheConcurrencyStrategy.READ_WRITE)
 @Table(name = "category")
 public class Category {
 
